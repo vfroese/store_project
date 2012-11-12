49a1f6a030a3b27bb3cdbc297bf1a0c33bbff5ea
Tori Froese <tori.froese@hotmail.com>
2012-11-12 11:57:44 -0600

    Create new store folder and files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'e8daa99d2b068519702cfd1b07564e458ceee4b3fc86ed16d2f8d8999b13ca03d7fc328fd75e632e28e7f187a7b9831ebd1865633c760dd214329fa7cb024d3c'
